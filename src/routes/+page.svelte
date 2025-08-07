33056a11509c1601ebcc19424a3f71e8f3083ceb
harizafiq <harizafiq0212@gmail.com>
2025-08-07 17:29:29 +0800

    Initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,11 @@
+<script>
+    let todos = [];
+
+    // Example: Replace with your Railway Go URL
+    const apiURL = 'https://to-do-app-production-23c0.up.railway.app/todos';
+
+    fetch(apiURL)
+        .then(res => res.json())
+        .then(data => todos = data)
+        .catch(console.error);
+</script>
